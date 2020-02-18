41a0bff75e9a65c84bb319d7205b454b9228df58
CN23 <codenation@CN23s-MacBook-Pro.local>
2020-02-18 16:19:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    inputText: "",
+    items: []
+  };
+
+  changeHandler = e => {
+    this.setState({ inputText: e.target.value });
+  };
+
+  removeHandler = index => {
+    let listItems = this.state.items;
+    listItems.splice(index, 1);
+    this.setState({ items: listItems });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      inputText: "",
+      items: [...this.state.items, this.state.inputText]
+    });
+  };
+
+  render() {
+    console.log(this.state.items);
+    return (
+      <div className="container">
+        <div className="board">
+          <h1 class="header">My To Do List</h1>
+          <div className="button">
+            <form onSubmit={this.onSubmit}>
+              <input
+                className="input"
+                type="text"
+                onChange={event => this.changeHandler(event)}
+                value={this.state.inputText}
+              ></input>
+              <button className="submit">click to add</button>
+            </form>
+          </div>
+          <div className="list">
+            <List items={this.state.items} removeHandler={this.removeHandler} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+const List = props => (
+  <ul>
+    {props.items.map((item, index) => (
+      <li key={index}>
+        {item}
+        <button className="delete" onClick={() => props.removeHandler(index)}>
+          x
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
