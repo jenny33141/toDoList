5cb55079119edef589856fd359771a228abaa5c4
CN23 <codenation@CN23s-MacBook-Pro.local>
2020-02-19 14:25:21 +0000

    Updates

---
+++ precache-manifest.99b4dc2058bdc9895c1ffeaf62b87ead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2294d901042efe00109719b47fde6d",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "a1da9e493881c4db28f3",
+    "url": "/toDoList/static/css/main.a24d354d.chunk.css"
+  },
+  {
+    "revision": "5be5d9aba402de87f491",
+    "url": "/toDoList/static/js/2.ed95b43e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/toDoList/static/js/2.ed95b43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1da9e493881c4db28f3",
+    "url": "/toDoList/static/js/main.da817a6a.chunk.js"
+  },
+  {
+    "revision": "76c06b5b3f8b483ebedf",
+    "url": "/toDoList/static/js/runtime-main.c78a27f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
